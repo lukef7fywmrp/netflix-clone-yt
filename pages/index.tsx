13d376b4250127c8995edb7eb872d3ca08df6812
lukef7fywmrp <lukef7fywmrp@gmail.com>
2022-04-13 17:18:26 +0400

    fixed a bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,11 @@ const Home = ({
   if (loading) return null
 
   return (
-    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
+    <div
+      className={`relative h-screen bg-gradient-to-b lg:h-[140vh] ${
+        showModal && '!h-screen overflow-hidden'
+      }`}
+    >
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
