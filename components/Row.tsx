a7c85d5fda719a4e15ba007ef9c01ce21e39aa76
lukef7fywmrp <lukef7fywmrp@gmail.com>
2022-04-17 14:20:08 +0400

    fixed a bug

--- components/Row.tsx
+++ components/Row.tsx
@@ -6,7 +6,7 @@ import Thumbnail from './Thumbnail'
 
 interface Props {
   title: string
-  movies: Movie | DocumentData[]
+  movies: Movie[] | DocumentData[]
 }
 
 function Row({ title, movies }: Props) {
